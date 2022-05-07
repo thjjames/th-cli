655d959884c816b330a741b5768bd0ea02d6030d
Haojun Tian <haojun.tian@shopee.com>
2022-05-07 17:38:20 +0800

    improve

--- bin/th-cli.js
+++ bin/th-cli.js
@@ -7,64 +7,29 @@ const requiredNodeVersion = semver.minVersion(require('../package.json').engines
 const cliName = require('../package.json').name;
 
 if (semver.lt(currentNodeVersion, requiredNodeVersion)) {
-    console.log(chalk.red(`You are running Node ${currentNodeVersion}.`));
-    console.log(chalk.red(`${cliName} requires Node ${requiredNodeVersion} or higher.`));
-    console.log(chalk.red(`Please upgrade your Node version.`));
-    process.exit(1);
+  console.error(chalk.red(`You are running Node ${currentNodeVersion}.`));
+  console.error(chalk.red(`${cliName} requires Node ${requiredNodeVersion} or higher.`));
+  console.error(chalk.red(`Please upgrade your Node version.`));
+  process.exit(1);
 }
 
 const program = require('commander');
-const fs = require('fs-extra');
-const path = require('path');
-const validatePackageName = require('validate-npm-package-name');
-const util = require('../util');
+const minimist = require('minimist');
 
 program
-    .version(require('../package.json').version, '-v, --version')
-    .usage('<command> [options]')
-    // output help information on unknown commands
-    // .arguments('<command>')
-    .action(cmd => {
-        console.log(chalk.red(`Unknown command ${chalk.bold(cmd)}.`))
-        program.outputHelp()
-    });
+  .version(require('../package.json').version, '-v, --version')
+  .usage('<command> [options]')
 
 program
-    .command('create <project-name>')
-    .description('create a new project by th-cli')
-    .option('-f, --force', 'overwrite target directory if it exists')
-    .action((name, cmd) => {
-        create(name, util.cleanArgs(cmd));
-    });
+  .command('create <project-name>')
+  .description('create a new project by th-cli')
+  .option('-f, --force', 'overwrite target directory if it exists')
+  .action((name, options) => {
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.warn(chalk.yellow('You provided more than one argument.'))
+      console.warn(chalk.yellow('The first one will be used as the project name, the rest are ignored.'))
+    }
+    require('../lib/create')(name, options);
+  });
 
 program.parse(process.argv);
-
-if (!process.argv.slice(2).length) {
-    program.outputHelp();
-}
-
-function create(name, option) {
-    const rootDir = path.resolve(name);
-    const templateDir = path.join(__dirname, '../template');
-    const baseName = path.basename(name);
-
-    if (!validatePackageName(baseName).validForNewPackages) {
-        console.log(chalk.red(`Could not create a project called ${baseName} because of npm naming restrictions.`));
-        process.exit(1);
-    }
-
-    console.log(chalk.bold(`Creating a new project in ${rootDir}...`));
-    if (fs.existsSync(rootDir)) {
-        if (option.force) {
-            fs.removeSync(rootDir);
-            fs.copySync(templateDir, rootDir);
-        } else {
-            console.log(chalk.red(`Folder ${rootDir} is already in use, please rename it or overwrite it by using option -f.`));
-            process.exit(1);
-        }
-    } else {
-        fs.ensureDirSync(rootDir);
-        fs.copySync(templateDir, rootDir);
-    }
-    console.log(chalk.bold('Completed'));
-}
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-`th-cli create <project-name>`
+# th-cli
+```bash
+npm install -g @github.com:thjjames/th-cli
+th-cli create <project-name>
+```
+
+## contributor
+tianhaojun <thjjames@163.com>
+
+## feadback
+please create issues on https://github.com/thjjames/th-cli/issues or send an email on <thjjames@163.com>
